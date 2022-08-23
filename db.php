32cea4703d4134882302516c597eafcf8f3f1c03
Palash Patil <97968344+PatilPalash@users.noreply.github.com>
2022-08-23 06:08:34 -0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+$serever = 'localhost';
+$username = 'postgres';
+$password = 'Abcd@1234';
+
+$dbconn = pg_connect("host=$serever port=5432 dbname=$db_name user=$username password=$password");
+
+
+
+?>
